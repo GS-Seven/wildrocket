69135c363c36faa804968a50b249514d3d4ba04b
jeffcail <cljcailianjie@163.com>
2023-11-09 14:05:51 +0800

    数组切片合集、并集、差集

--- wutils/slice.go
+++ wutils/slice.go
@@ -2,8 +2,8 @@ package wutils
 
 type WUtilSlice struct{}
 
-// StringsInter 取两个字符串列表的交集
-func (w *WUtilSlice) StringsInter(s1, s2 []string) []string {
+// Intersect 取两个列表的交集
+func (w *WUtilSlice) Intersect(s1, s2 []string) []string {
 	m := map[string]bool{}
 	for _, v := range s1 {
 		m[v] = true
@@ -15,4 +15,68 @@ func (w *WUtilSlice) StringsInter(s1, s2 []string) []string {
 		}
 	}
 	return res
+}
+
+// DifferentSet 取两个列表的差集
+func (w *WUtilSlice) DifferentSet(s1, s2 []string) []string {
+	m := map[string]string{}
+	var res []string
+
+	for _, v := range s1 {
+		if _, ok := m[v]; !ok {
+			m[v] = v
+		}
+	}
+
+	for _, v := range s2 {
+		if _, ok := m[v]; !ok {
+			res = append(res, v)
+		}
+	}
+
+	return res
+}
+
+// Merge 合集
+func (w *WUtilSlice) Merge(s1, s2 []string) []string {
+	var res []string
+	for _, v := range s1 {
+		res = append(res, v)
+	}
+
+	for _, v := range s2 {
+		res = append(res, v)
+	}
+	return res
+}
+
+// MergeRepeatedElement 合集去重
+func (w *WUtilSlice) MergeRepeatedElement(s1, s2 []string) []string {
+	var res []string
+	for _, v := range s1 {
+		res = append(res, v)
+	}
+
+	for _, v := range s2 {
+		res = append(res, v)
+	}
+	return w.RemoveRepeatedElement(res)
+}
+
+// RemoveRepeatedElement 数组切片去重
+func (w *WUtilSlice) RemoveRepeatedElement(arr []string) (newArr []string) {
+	newArr = make([]string, 0)
+	for i := 0; i < len(arr); i++ {
+		repeat := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return
 }
--- wutils/slice_test.go
+++ wutils/slice_test.go
@@ -15,6 +15,47 @@ func aa() {
 	w := &WUtilSlice{}
 	s1 := []string{"a", "b", "c"}
 	s2 := []string{"a", "c"}
-	intersect := w.StringsInter(s1, s2)
+	intersect := w.Intersect(s1, s2)
 	fmt.Println(intersect)
+}
+
+func TestWUtilSlice_DifferentSet(t *testing.T) {
+	w := &WUtilSlice{}
+	s1 := []string{"a", "b"}
+	s2 := []string{"a", "b", "c"}
+	intersect := w.DifferentSet(s1, s2)
+	fmt.Println(intersect)
+}
+
+func TestWUtilSlice_Merge(t *testing.T) {
+	w := &WUtilSlice{}
+	s1 := []string{"a", "b"}
+	s2 := []string{"a", "b", "c"}
+	merge := w.Merge(s1, s2)
+	fmt.Println(merge)
+}
+
+func TestWUtilSlice_MergeRepeatedElement(t *testing.T) {
+	w := &WUtilSlice{}
+	s1 := []string{"a", "b"}
+	s2 := []string{"a", "b", "c"}
+	merge := w.MergeRepeatedElement(s1, s2)
+	fmt.Println(merge)
+}
+
+func TestWUtilSlice_RemoveRepeatedElement(t *testing.T) {
+	//s1 := []string{"a", "b", "c", "a", "b", "1", "1"}
+	//w := &WUtilSlice{}
+	//arr := w.RemoveRepeatedElement(s1)
+	//fmt.Println(arr)
+	for i := 0; i < 1000; i++ {
+		go bb()
+	}
+}
+
+func bb() {
+	s1 := []string{"a", "b", "c", "a", "b", "1", "1"}
+	w := &WUtilSlice{}
+	arr := w.RemoveRepeatedElement(s1)
+	fmt.Println(arr)
 }
